06c2e3c3a2eafaa071dcd5905aa436fe9982b83f
Sandziso Kunene <ict.sandziso@gmail.com>
2025-11-30 21:57:16 +0200

    Initial MAUI WebView project for Africa Chicks Shopping

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace SmartstoreApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+    }
+}
+
